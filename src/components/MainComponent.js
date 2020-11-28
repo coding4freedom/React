060f4f0ebd43efa5177e061c6d55d6fd7ed6d499
chris <chawkins2010@hotmail.com>
2020-11-28 13:23:35 -0600

    week 3 workshop

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import Directory from './DirectoryComponents';
+import About  from './AboutComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
@@ -53,6 +54,7 @@ class Main extends Component {
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites}
                     />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                    <Route exact path='/aboutus'render={() => <About partners={this.state.partners} />} />
                     <Route exact path="/contactus" component={Contact} />
                     <Redirect to='/home' />                                     
                 </Switch>              
