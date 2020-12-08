d1bec11c6b64e17c41aba83d3a65337e8a9e6098
Chris Hawkins <chawkins2010@hotmail.com>
2020-12-07 18:24:48 -0600

    Redux actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 const maxLength = len => val => !(val) || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
+
 function RenderCampsite({campsite}) {
     return (
         <div className="col-md-5 m-1">
@@ -17,7 +18,7 @@ function RenderCampsite({campsite}) {
         </div>
     );
 }
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -34,7 +35,7 @@ function RenderComments({comments}) {
                         );
                     })
                 }
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         );
     }
@@ -54,10 +55,9 @@ class CommentForm extends Component {
             isModalOpen: !this.state.isModalOpen
         });
     }
-    handleSubmit(values) {
+    handleSubmit(values) {        
         this.toggleModal();
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);        
     }
     render() {
         return (
@@ -134,7 +134,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,10 +6,12 @@ import Contact from './ContactComponent';
 import Directory from './DirectoryComponents';
 import About  from './AboutComponent';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
 import CampsiteInfo from './CampsiteInfoComponent';
 
-const mappropsToProps = state => {
+
+const mapStateToProps = state => {
     return{
         campsites: state.campsites,
         comments: state. comments,
@@ -17,6 +19,10 @@ const mappropsToProps = state => {
         promotions: state.promotions
     }
 }
+
+const mapDispatchToProps = {
+    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text))
+};
 
 class Main extends Component {
 
@@ -26,10 +32,9 @@ class Main extends Component {
         const CampsiteWithId = ({match}) => {
             return (
                 <CampsiteInfo 
-                    campsite={this.props.campsites.filter(campsite => campsite.id ===
-                     +match.params.campsiteId)[0]}
-                    comments={this.props.comments.filter(comment => comment.campsiteId ===
-                        +match.params.campsiteId)}
+                    campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                    addComment={this.props.addComment}
                 />
             );
         }
@@ -60,5 +65,4 @@ class Main extends Component {
         );
     }
 }
-
-export default withRouter(connect(mappropsToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
